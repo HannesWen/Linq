0fdfc5878bcc2e7775704c6531427d483e7e7fee
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-28 10:48:51 +0200

    first 3 tasks

--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -10,6 +10,50 @@ namespace Linq
     {
         static void Main(string[] args)
         {
+            //Arbeitsblatt Aufgaben zu Linq Nr. 1 Gebe aus seiner Liste von Zahlen nur die Zahlen aus, welche durch 2 Teilbar sind
+            //List<int> liste = new List<int>() { 1, 4, 5, 8, 9, 12 };
+            //var neueListe = from gerade in liste
+            //                where gerade % 2 == 0
+            //                select gerade;
+            //foreach (var gerade in neueListe)
+            //{
+            //    Console.WriteLine(gerade);
+            //}
+            //Console.ReadKey();
+
+            //Arbeitsblatt Aufgaben zu Linq Nr. 2 Gebe aus einer Liste von Zahlen nur die ungeraden Werte aus
+            //List<int> liste = new List<int>() { 1, 4, 5, 8, 9, 12 };
+            //var neueliste = from ungerade in liste
+            //                where ungerade % 2 == 1
+            //                select ungerade;
+            //foreach (var ungerade in neueliste)
+            //{
+            //    Console.WriteLine(ungerade);
+            //}
+            //Console.ReadKey();
+
+            //rbeitsblatt Aufgaben zu Linq Nr. 3 Gebe für die angegebenen Zahlen , die Zahl sowie die Quadratzahl aus, wobei die Quadratzahl größer als 20 sein soll var arr1 = new[] { 3, 9, 2, 8, 6, 5 };
+            //var arr1 = new[] { 3, 9, 2, 8, 6, 5 };
+            //foreach (var nummer in arr1)
+            //{
+            //    Console.WriteLine(nummer);
+            //}
+            //var newarr1 = from zahl in arr1
+            //              where zahl*zahl >= 20
+            //              select zahl;
+            //foreach (var zahl in newarr1)
+            //{
+            //    Console.WriteLine(zahl*zahl);
+            //}
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
         }
     }
 }
