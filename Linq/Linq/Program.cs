7264f4ec3872802a1400b190fa6d9d8a285faa3d
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-28 11:38:53 +0200

    no message

--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -32,7 +32,7 @@ namespace Linq
             //}
             //Console.ReadKey();
 
-            //rbeitsblatt Aufgaben zu Linq Nr. 3 Gebe für die angegebenen Zahlen , die Zahl sowie die Quadratzahl aus, wobei die Quadratzahl größer als 20 sein soll var arr1 = new[] { 3, 9, 2, 8, 6, 5 };
+            //Arbeitsblatt Aufgaben zu Linq Nr. 3 Gebe für die angegebenen Zahlen , die Zahl sowie die Quadratzahl aus, wobei die Quadratzahl größer als 20 sein soll var arr1 = new[] { 3, 9, 2, 8, 6, 5 };
             //var arr1 = new[] { 3, 9, 2, 8, 6, 5 };
             //foreach (var nummer in arr1)
             //{
@@ -47,12 +47,28 @@ namespace Linq
             //}
             //Console.ReadKey();
 
+            //Arbeitsblatt Aufgaben zu Linq Nr. 4 Gehe eine Liste von zahlen durch, und gebe die Zahl sowie die Häufigkeit, wie oft diese in der Liste auftritt int[] arr1 = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            //int[] arr1 = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            //var arr2 = from zahl in arr1
+            //           group zahl by zahl into anzahl
+            //           select anzahl;
+            //foreach (var arrNo in arr2)
+            //{
+            //    Console.WriteLine("Number " + arrNo.Key + " appears " + arrNo.Count() + " times");
+            //}
+            //Console.ReadKey();
 
 
-
-
-
-
+            //Arbeitsblatt Aufgaben zu Linq Nr. 5 Gebe die Häufigkeit jeden Buchstabens des angegebenen Strings aus. Lasse den Nutzer ein eigenes Wort eingeben. string str = „w3resource“;
+            string str = "w3resource";
+            var str1 = from x in str
+                       group x by x into buchstabe
+                       select buchstabe;
+            foreach (var x in str1)
+            {
+                Console.WriteLine("Buchstabe " + x.Key + " anzahl " + x.Count());
+            }
+            Console.ReadKey();
 
         }
     }
